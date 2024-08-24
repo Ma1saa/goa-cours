c7b3becf5c9359ff708df9fa10176d330bb0dc11
Ma1saa <maisuradzemate7@gmail.com>
2024-08-24 15:59:04 +0400

    Add files via upload

---
+++ GOA-COURS/day 4/classwork.py
@@ -0,0 +1,14 @@
+my_name = "mate"
+print(my_name)
+
+age = 14
+print(age)
+
+print((5 + 6) * 9 / 3)
+
+#ცვლადის სახელი უნდა იყოს ქვედა ტირეთი დაშორებული და ცვლადი პრჭყალებში უნდა იყოს
+my name = mate
+print(my name)
+
+name = "mate" #ეს არის str (string) ტიპის ცვლადი
+age = 14 #ეს არის int (integer) მთელი რიცხვი
---
+++ GOA-COURS/day 8/homework.py
@@ -0,0 +1,40 @@
+print(57 > 46)
+print(59 < 68)
+print(61 <= 62)
+print(5 == 6)
+print(5 != 6)
+print(5 == 5)
+print(19 >= 6)
+print(46 >= 46)
+print(19 < 46)
+print(46 != 68)
+print(19 <= 19)
+print(67 > 55)
+
+age = 14
+print(type(age))
+
+weight = 57.7
+print(type(weight))
+
+user_age = "42"
+print(int(user_age))
+
+num = "3.14"
+print(float(num))
+
+user_name = input("Enter your name: ")
+print(type(user_name))
+
+user_age = int(input("Enter your age: "))
+print(type(user_age))
+
+user_weight = float(input("Enter your weight: "))
+print(type(user_weight))
+
+user_age1 = int(input("Enter your age: "))
+print(2024 - user_age1)
+
+user_age2 = int(input("Enter your age: "))
+dad_age = int(input("Enter your dads age: "))
+print((dad_age + 15)/(user_age2 + 15))
