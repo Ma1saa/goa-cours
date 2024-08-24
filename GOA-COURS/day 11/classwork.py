9c07b7ae68d58753f74a20414a4897ad7bc30767
Ma1saa <maisuradzemate7@gmail.com>
2024-08-24 15:59:50 +0400

    Add files via upload

---
+++ GOA-COURS/day 11/classwork.py
@@ -0,0 +1,21 @@
+for i in range(1, 100):
+    print(i)
+
+for i in range(5, 50, 5):
+    print(i)
+
+for i in range(50):
+    print("GOA BEST")
+
+for i in range(0, 25):
+    print(i+2)
+
+for i in range(0, 100):
+    print(i*2)
+
+for i in range(1, 500, 10):
+    print(i*25)
+
+name = input("Enter your name: ")
+for i in range(0, 16):
+    print(str(i) + name)
